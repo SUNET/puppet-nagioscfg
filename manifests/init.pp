adae289e24b3a8aac624ae08965c98edcca9792c
Leif Johansson <leifj@sunet.se>
2015-08-13 23:12:30 +0200

    nagios servic

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,6 @@
 class nagioscfg($hostgroups={}, $cfgdir='/etc/nagios3', $host_template='host', $config = "nagioscfg") {
+  package {'nagios3': ensure => present }
+  service {'nagios3': ensure => running }
   if has_key($hostgroups,'all') {
     each($hostgroups['all']) |$hostname| {
       notify {"generating ${hostname}": }
