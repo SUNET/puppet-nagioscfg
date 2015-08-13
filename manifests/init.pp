97169c6b78d3d1b7e4845933e3eef15cafdbf9c0
Leif Johansson <leifj@sunet.se>
2015-08-13 23:36:55 +0200

    nagios servic

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,26 @@
 class nagioscfg($hostgroups={}, $cfgdir='/etc/nagios3', $host_template='host', $config = "nagioscfg") {
   package {'nagios3': ensure => present }
   service {'nagios3': ensure => running }
+  concat {"${cfgdir}/${config}_hostgroups.cfg":
+    owner => root,
+    group => root,
+    mode  => '0644'
+  }
+  concat {"${cfgdir}/${config}_hosts.cfg":
+    owner => root,
+    group => root,
+    mode  => '0644'
+  }
+  concat {"${cfgdir}/${config}_servicegroups.cfg":
+    owner => root,
+    group => root,
+    mode  => '0644'
+  }
+  concat {"${cfgdir}/${config}_services.cfg":
+    owner => root,
+    group => root,
+    mode  => '0644'
+  }
   if has_key($hostgroups,'all') {
     each($hostgroups['all']) |$hostname| {
       notify {"generating ${hostname}": }
