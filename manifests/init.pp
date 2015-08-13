65e7b7334364b1b0d1d4afcdd659f5f8a3beb61b
Leif Johansson <leifj@sunet.se>
2015-08-13 23:47:15 +0200

    cleanup config

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class nagioscfg($hostgroups={}, $cfgdir='/etc/nagios3', $host_template='host', $config = "nagioscfg") {
+class nagioscfg($hostgroups={}, $cfgdir='/etc/nagios3/conf.d', $host_template='generic-host', $config = "nagioscfg") {
   package {'nagios3': ensure => present }
   service {'nagios3': ensure => running }
   concat {"${cfgdir}/${config}_hostgroups.cfg":
