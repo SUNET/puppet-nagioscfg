52841e6967f44ec777ec67696a2cd522200eadb8
Leif Johansson <leifj@sunet.se>
2015-08-13 22:35:05 +0200

    alternative invokation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class nagioscfg($hostgroups={}, $cfgdir='/etc/nagios3', $host_template='host', $config = "nagioscfg") {
   if has_key($hostgroups,'all') {
-    $hostgroups['all'].each |$hostname| {
+    each($hostgroups['all']) |$hostname| {
       nagioscfg::host {$hostname: }
     }
   }
