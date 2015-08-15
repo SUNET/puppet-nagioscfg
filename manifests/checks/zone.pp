73c89096ba0d763b01e3299e4e6aa990bee20b98
Leif Johansson <leifj@sunet.se>
2015-08-15 17:58:25 +0200

    allow for longer response times

--- manifests/checks/zone.pp
+++ manifests/checks/zone.pp
@@ -1,26 +1,26 @@
 define nagioscfg::checks::zone ($hostgroup_name = []) {
   nagioscfg::command {"check_zone_${name}_4":
-    command_line => "/usr/lib/nagios/plugins/check_dig -4 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$'"
+    command_line => "/usr/lib/nagios/plugins/check_dig -4 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$' -w 0.5 -c 1"
   }
   nagioscfg::command {"check_zone_${name}_6":
-    command_line => "/usr/lib/nagios/plugins/check_dig -6 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$'"
+    command_line => "/usr/lib/nagios/plugins/check_dig -6 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$' -w 0.5 -c 1"
   }
-  nagioscfg::service {"${name}_SOA":
+  nagioscfg::service {"${name}_SOA_4":
     check_command  => "check_zone_${name}_4!${name}!SOA",
     hostgroup_name => $hostgroup_name,
     description    => "${name} SOA"
   }
-  nagioscfg::service {"${name}_SOA":
+  nagioscfg::service {"${name}_SOA_6":
     check_command  => "check_zone_${name}_6!${name}!SOA",
     hostgroup_name => $hostgroup_name,
     description    => "${name} SOA"
   }
-  nagioscfg::service {"${name}_NS":
+  nagioscfg::service {"${name}_NS_4":
     check_command  => "check_zone_${name}_4!${name}!NS",
     hostgroup_name => $hostgroup_name,
     description    => "${name} NS"
   }
-  nagioscfg::service {"${name}_NS":
+  nagioscfg::service {"${name}_NS_6":
     check_command  => "check_zone_${name}_6!${name}!NS",
     hostgroup_name => $hostgroup_name,
     description    => "${name} NS"
