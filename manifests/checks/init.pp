a6183ad866ca0ba7f64265583e5340cf31291d35
Leif Johansson <leifj@sunet.se>
2015-08-16 00:11:53 +0200

    cleanup

--- manifests/checks/init.pp
+++ manifests/checks/init.pp
@@ -1,9 +1,2 @@
 
-class nagioscfg::checks {
-  nagioscfg::command {"check_zone_4":
-    command_line => "/usr/lib/nagios/plugins/check_dig -4 -H '\$HOSTNAME\$' -l '\$ARG1\$' -T '\$ARG2\$'"
-  }
-  nagioscfg::command {"check_zone_6":
-    command_line => "/usr/lib/nagios/plugins/check_dig -6 -H '\$HOSTNAME\$' -l '\$ARG1\$' -T '\$ARG2\$'"
-  }
-}
+class nagioscfg::checks { }
--- manifests/checks/zone.pp
+++ manifests/checks/zone.pp
@@ -1,6 +1,13 @@
 
-class { 'nagioscfg::checks': }
+import stdlib
 define nagioscfg::checks::zone ($hostgroup_name = []) {
+
+  ensure_resource('nagioscfg::command','check_zone_4', {
+    command_line => "/usr/lib/nagios/plugins/check_dig -4 -H '\$HOSTNAME\$' -l '\$ARG1\$' -T '\$ARG2\$'"
+  })
+  ensure_resource('nagioscfg::command','check_zone_6', {
+    command_line => "/usr/lib/nagios/plugins/check_dig -6 -H '\$HOSTNAME\$' -l '\$ARG1\$' -T '\$ARG2\$'"
+  })
   nagioscfg::service {"${name}_SOA_4":
     check_command  => "check_zone_4!${name}!SOA",
     hostgroup_name => $hostgroup_name,
