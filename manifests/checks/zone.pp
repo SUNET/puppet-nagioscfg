9d2438d9d89ee88cf883f14993b16608ce37772a
Leif Johansson <leifj@sunet.se>
2015-08-15 18:02:09 +0200

    allow for longer response times

--- manifests/checks/zone.pp
+++ manifests/checks/zone.pp
@@ -1,9 +1,9 @@
 define nagioscfg::checks::zone ($hostgroup_name = []) {
   nagioscfg::command {"check_zone_${name}_4":
-    command_line => "/usr/lib/nagios/plugins/check_dig -4 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$' -w 0.5 -c 1"
+    command_line => "/usr/lib/nagios/plugins/check_dig -4 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$' -w 400 -c 600"
   }
   nagioscfg::command {"check_zone_${name}_6":
-    command_line => "/usr/lib/nagios/plugins/check_dig -6 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$' -w 0.5 -c 1"
+    command_line => "/usr/lib/nagios/plugins/check_dig -6 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$' -w 400 -c 600"
   }
   nagioscfg::service {"${name}_SOA_4":
     check_command  => "check_zone_${name}_4!${name}!SOA",
