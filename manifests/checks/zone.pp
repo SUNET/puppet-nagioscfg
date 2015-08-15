efdd5b213d5f41e8696d6bba1b082e0e7fa9fd8d
Leif Johansson <leifj@sunet.se>
2015-08-16 00:05:00 +0200

    cleanup

--- manifests/checks/zone.pp
+++ manifests/checks/zone.pp
@@ -1,27 +1,21 @@
 define nagioscfg::checks::zone ($hostgroup_name = []) {
-  nagioscfg::command {"check_zone_${name}_4":
-    command_line => "/usr/lib/nagios/plugins/check_dig -4 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$'"
-  }
-  nagioscfg::command {"check_zone_${name}_6":
-    command_line => "/usr/lib/nagios/plugins/check_dig -6 -H '\$HOSTNAME$' -l ${name} -T '\$ARG1\$'"
-  }
   nagioscfg::service {"${name}_SOA_4":
-    check_command  => "check_zone_${name}_4!${name}!SOA",
+    check_command  => "check_zone_4!${name}!SOA",
     hostgroup_name => $hostgroup_name,
     description    => "${name} SOA v4"
   }
   nagioscfg::service {"${name}_SOA_6":
-    check_command  => "check_zone_${name}_6!${name}!SOA",
+    check_command  => "check_zone_6!${name}!SOA",
     hostgroup_name => $hostgroup_name,
     description    => "${name} SOA v6"
   }
   nagioscfg::service {"${name}_NS_4":
-    check_command  => "check_zone_${name}_4!${name}!NS",
+    check_command  => "check_zone_4!${name}!NS",
     hostgroup_name => $hostgroup_name,
     description    => "${name} NS v4"
   }
   nagioscfg::service {"${name}_NS_6":
-    check_command  => "check_zone_${name}_6!${name}!NS",
+    check_command  => "check_zone_6!${name}!NS",
     hostgroup_name => $hostgroup_name,
     description    => "${name} NS v6"
   }
