1ad3a15414a901d91c9604d15560369b87918b7a
Leif Johansson <leifj@sunet.se>
2015-08-18 00:12:48 +0200

    check in body

--- manifests/checks/uri_content.pp
+++ manifests/checks/uri_content.pp
@@ -2,7 +2,7 @@ include stdlib
 
 define nagioscfg::checks::uri_content($match = undef) {
   ensure_resource('nagioscfg::command', 'check_http_content', {
-      command_line   => '/usr/lib/nagios/plugins/check_http -H \'$HOSTNAME$\' -e \'$ARG1$\''
+      command_line   => '/usr/lib/nagios/plugins/check_http -H \'$HOSTNAME$\' -u / -s \'$ARG1$\''
   })
   $f = fqdn_rand(1000, $name)
   nagioscfg::service {"check_${name}_${f}":
