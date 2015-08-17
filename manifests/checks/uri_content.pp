f71012c3eb3edfb33b79c0ab9fcf9e8427e6b8f5
Leif Johansson <leifj@sunet.se>
2015-08-17 23:23:26 +0200

    unique

--- manifests/checks/uri_content.pp
+++ manifests/checks/uri_content.pp
@@ -1,10 +1,13 @@
+import stdlib
+
 define nagioscfg::checks::uri_content($match = undef) {
   ensure_resource('nagioscfg::command', 'check_http_content', {
       command_line   => '/usr/lib/nagios/plugins/check_http -H \'$HOSTNAME\$\' -e \'$ARG1$\''
   })
-  nagioscfg::service {'check_${name}_uri_content':
+  $f = fqdn_rand(1000, ${name})
+  nagioscfg::service {'check_${name}_${f}':
     host_name      => [$name],
     check_command  => "check_http_content!${match}",
-    description    => "http://${name}/ ('${match}')"
+    description    => "http://${name}/ contains '${match}'"
   }
 }
