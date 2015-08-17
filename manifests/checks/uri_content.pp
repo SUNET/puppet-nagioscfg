90f30d455bc64b9ece8f88b8c6563f3d58a1bc5c
Leif Johansson <leifj@sunet.se>
2015-08-18 00:47:31 +0200

    make uri configurable

--- manifests/checks/uri_content.pp
+++ manifests/checks/uri_content.pp
@@ -2,12 +2,12 @@ include stdlib
 
 define nagioscfg::checks::uri_content($match = undef, $uri = '/') {
   ensure_resource('nagioscfg::command', 'check_http_content', {
-      command_line   => "/usr/lib/nagios/plugins/check_http -H '\$HOSTNAME\$' -u '${uri}' -s '\$ARG1\$'"
+      command_line   => "/usr/lib/nagios/plugins/check_http -H '\$HOSTNAME\$' -u '\$ARG2\$' -s '\$ARG1\$'"
   })
-  $f = fqdn_rand(1000, $name)
-  nagioscfg::service {"check_${name}_${f}":
+  $f = fqdn_rand(1000, "${name}_${uri}")
+  nagioscfg::service {"check_${name}_${uri}_${f}":
     host_name      => [$name],
-    check_command  => "check_http_content!${match}",
-    description    => "http://${name}/ contains ${match}"
+    check_command  => "check_http_content!${match}!${uri}",
+    description    => "http://${name}${uri} contains ${match}"
   }
 }
