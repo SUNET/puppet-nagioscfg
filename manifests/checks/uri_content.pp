5793959a72d04e2d2ad269f361bf863d091d4f68
Leif Johansson <leifj@sunet.se>
2015-08-17 23:11:21 +0200

    uri content check

--- manifests/checks/uri_content.pp
+++ manifests/checks/uri_content.pp
@@ -1,4 +1,7 @@
 define check_uri_content($match = undef) {
+  ensure_resource('nagioscfg::command', 'check_http_content', {
+      command_line   => '/usr/lib/nagios/plugins/check_http -H \'$HOSTNAME\$\' -e \'$ARG1$\''
+  })
   nagioscfg::service {'check_${name}_uri_content':
     host_name      => [$name],
     check_command  => "check_http_content!${match}",
