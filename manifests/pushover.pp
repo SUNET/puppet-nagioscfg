ed2eaacccf7ef4e7c083ca91e2e1111c578d0777
Leif Johansson <leifj@sunet.se>
2016-01-30 23:56:44 +0100

    unescape more newlines

--- manifests/pushover.pp
+++ manifests/pushover.pp
@@ -7,6 +7,6 @@ class nagioscfg::pushover {
      content => template('nagioscfg/notify_by_pushover.erb')
    }
    nagioscfg::command { 'notify-host-by-pushover': 
-     command_line => "/usr/lib/nagios/plugins/notify_by_pushover -u '\$CONTACTADDRESS1\$' -a '\$CONTACTADDRESS2\$' -t '\$NOTIFICATIONTYPE\$ Host Alert: \$HOSTNAME\$ is \$HOSTSTATE\$' -m 'Notification Type: \$NOTIFICATIONTYPE\$\nHost: \$HOSTNAME\$\nState: \$HOSTSTATE\$\nAddress: \$HOSTADDRESS\$\nInfo: \$HOSTOUTPUT\$\n\nDate/Time: \$LONGDATETIME\$\n'"
+     command_line => "/usr/lib/nagios/plugins/notify_by_pushover -u '\$CONTACTADDRESS1\$' -a '\$CONTACTADDRESS2\$' -t '\$NOTIFICATIONTYPE\$ Host Alert: \$HOSTNAME\$ is \$HOSTSTATE\$' -m 'Notification Type: \$NOTIFICATIONTYPE\$\\nHost: \$HOSTNAME\$\\nState: \$HOSTSTATE\$\\nAddress: \$HOSTADDRESS\$\\nInfo: \$HOSTOUTPUT\$\\n\\nDate/Time: \$LONGDATETIME\$\\n'"
    }
 }
