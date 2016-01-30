6f9d8e35a38da203731e63003f8a1c387f346ced
Leif Johansson <leifj@sunet.se>
2016-01-30 23:54:01 +0100

    join hell

--- manifests/contact.pp
+++ manifests/contact.pp
@@ -17,11 +17,11 @@ define nagioscfg::contact($ensure = 'present',
                           $can_submit_commands = '1'
 ) {
   $contact_groups_list = any2array($contact_groups)
-  $contact_groups_joined = join(",",$contact_groups_list)
+  $contact_groups_joined = join($contact_groups_list,',')
   $service_notification_commands_list = any2array($service_notification_commands)
-  $service_notification_commands_joined = join(",",$service_notification_commands_list)
+  $service_notification_commands_joined = join($service_notification_commands_list,',')
   $host_notification_commands_list = any2array($host_notification_commands)
-  $host_notification_commands_joined = join(",",$host_notification_commands_list)
+  $host_notification_commands_joined = join($host_notification_commands_list,',')
   concat::fragment {"${nagioscfg::config}_contact_${name}":
     target  => "${nagioscfg::cfgdir}/${nagioscfg::config}_contacts.cfg",
     content => template('nagioscfg/contact.erb'),
