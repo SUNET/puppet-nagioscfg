45ebecc7f7765ef8c970c8eb5faa44b220d65321
Leif Johansson <leifj@sunet.se>
2016-03-01 16:50:24 +0100

    bar!

--- manifests/passive.pp
+++ manifests/passive.pp
@@ -9,7 +9,7 @@ class nagioscfg::passive {
   } ->
   augeas { 'nagios_passive_monitor_settings':
     incl     => "/etc/nagios3/nagios.cfg",
-    lens     => 'nagioscfg',
+    lens     => 'NagiosCfg.lns',
     changes  => [
       "set enable_notifications 0",
       "set obsess_over_services 1",
