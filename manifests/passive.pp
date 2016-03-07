0452a3c6ff9302086c2c619c461aa492f5fecbdf
Leif Johansson <leifj@sunet.se>
2016-03-07 11:20:53 +0100

    insert nsca server and config in plugin command directly

--- manifests/passive.pp
+++ manifests/passive.pp
@@ -1,11 +1,15 @@
 class nagioscfg::passive {
   require augeas
   ensure_resource('package', 'nsca-client', { ensure => present })
+  $nsca_server = hiera("nsca-server");
+  $password = hiera("nsca-password");
+  $encryption_method = hiera("nsca-encryption-method");
+
   nagioscfg::command { "obsessive_host_handler":
-    command_line => '/usr/share/icinga/plugins/eventhandlers/distributed-monitoring/send_nsca_host_or_service_check_result \'$_HOSTOCHP_HOST$\' \'$_HOSTOCHP_CONFIG$\' \'$HOSTNAME$\' \'$HOSTSTATE$\' \'$HOSTOUTPUT$\\n$LONGHOSTOUTPUT$|$HOSTPERFDATA$\''
+    command_line => "/usr/share/icinga/plugins/eventhandlers/distributed-monitoring/send_nsca_host_or_service_check_result '${nsca_server}' '/etc/send_nsca.cfg' '\$HOSTNAME\$' '\$HOSTSTATE\$' '\$HOSTOUTPUT\$\\n\$LONGHOSTOUTPUT\$|\$HOSTPERFDATA\$'"
   } ->
   nagioscfg::command { "obsessive_service_handler":
-    command_line => '/usr/share/icinga/plugins/eventhandlers/distributed-monitoring/send_nsca_host_or_service_check_result \'$_SERVICEOCSP_HOST$\' \'$_SERVICEOCSP_CONFIG$\' \'$HOSTNAME$\' \'$SERVICEDESC$\' \'$SERVICESTATE$\' \'$SERVICEOUTPUT$\\n$LONGSERVICEOUTPUT$|$SERVICEPERFDATA$\''
+    command_line => "/usr/share/icinga/plugins/eventhandlers/distributed-monitoring/send_nsca_host_or_service_check_result '${nsca_server}' '/etc/send_nsca.cfg' '\$HOSTNAME\$' '\$SERVICEDESC\$' '\$SERVICESTATE\$' '\$SERVICEOUTPUT\$\\n\$LONGSERVICEOUTPUT\$|\$SERVICEPERFDATA\$'"
   } ->
   augeas { 'nagios_passive_monitor_settings':
     incl     => "/etc/nagios3/nagios.cfg",
@@ -17,13 +21,11 @@ class nagioscfg::passive {
       "set ocsp_command obsessive_service_handler",
       "set ochp_command obsessive_host_handler"
     ]
-  }
-  $password = hiera("nsca-password");
-  $encryption_method = hiera("nsca-encryption-method");
-  file {'/etc/send_nsca.conf':
+  } ->
+  file {'/etc/send_nsca.cfg':
      owner   => nagios,
      group   => nagios,
      mode    => '0400',
-     content => template("nagioscfg/send_nsca.cnf.erb")
+     content => template("nagioscfg/send_nsca.cfg.erb")
   }
 }
