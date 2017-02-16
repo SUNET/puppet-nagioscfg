ccc4c3251017366faab52f194a24ebe7b262110c
Leif Johansson <leifj@sunet.se>
2017-02-16 16:25:25 +0100

    variables for nagios core

--- manifests/slack.pp
+++ manifests/slack.pp
@@ -10,9 +10,9 @@ class nagioscfg::slack($domain=undef,$token=undef) {
 
 define nagioscfg::slack::channel() {
    nagioscfg::command {"notify-host-to-slack-$name":
-      command_line => "/usr/lib/nagios/plugins/slack_nagios.pl -field slack_channel=#$name"
+      command_line => "/usr/lib/nagios/plugins/slack_nagios.pl -field slack_channel=#$name -field HOSTALIAS='\$HOSTNAME\$' -field SERVICEDESC='\$SERVICEDESC\$' -field SERVICESTATE='\SERVICESTATE\$' -field SERVICEOUTPUT='\$SERVICEOUTPUT\$' -field NOTIFICATIONTYPE='\$NOTIFICATIONTYPE\$'"
    }
    nagioscfg::command {"notify-service-to-slack-$name":
-      command_line => "/usr/lib/nagios/plugins/slack_nagios.pl -field slack_channel=#$name"
+      command_line => "/usr/lib/nagios/plugins/slack_nagios.pl -field slack_channel=#$name -field HOSTALIAS='\$HOSTNAME\$' -field HOSTSTATE='\$HOSTSTATE\$' -field HOSTOUTPUT='\$HOSTOUTPUT\$' -field NOTIFICATIONTYPE='\$NOTIFICATIONTYPE\$'"
    }
 }
