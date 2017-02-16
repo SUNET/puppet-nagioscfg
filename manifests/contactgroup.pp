fdeaaeec57e565fd05a0c2fe1719d864e701b449
Leif Johansson <leifj@sunet.se>
2017-02-16 09:42:39 +0100

    implement 'ensure' keyword and support for contact groups

---
+++ manifests/contactgroup.pp
@@ -0,0 +1,24 @@
+include stdlib
+include concat
+
+define nagioscfg::contactgroup($cgalias = undef, $ensure = 'present', $members = undef) {
+  $contactgroup_alias = $cgalias ? {
+    undef   => $name,
+    default => $cgalias
+  }
+  $def_members = has_key($nagioscfg::contactgroups, $name) ? {
+    true  => $nagioscfg::contactgroups[$name],
+    false => undef,
+  }
+  $contactgroup_members = $members ? {
+    undef   => $def_members,
+    default => join($members, ',')
+  }
+  concat::fragment {"${nagioscfg::config}_contactgroup_${name}":
+    target  => "${nagioscfg::cfgdir}/${nagioscfg::config}_contactgroups.cfg",
+    content => template('nagioscfg/contactgroup.erb'),
+    order   => '30',
+    notify  => Service['nagios3'],
+    ensure  => $ensure
+  }
+}
