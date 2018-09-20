fe8fd3ffb7908e3da4e362cba07a379c36a877bb
Fredrik Thulin <fredrik@thulin.net>
2018-09-20 15:01:00 +0200

    eduid has a different path to nagios.cfg

--- manifests/passive.pp
+++ manifests/passive.pp
@@ -1,8 +1,9 @@
 class nagioscfg::passive (
-  $enable_notifications = '0',
-  $nsca_server          = hiera('nsca_server'),
-  $password             = hiera('nsca_password'),
-  $encryption_method    = hiera('nsca_encryption_method', '14'),  # 14 is AES-128
+  String $enable_notifications = '0',
+  String $nsca_server          = hiera('nsca_server'),
+  String $password             = hiera('nsca_password'),
+  String $encryption_method    = hiera('nsca_encryption_method', '14'),  # 14 is AES-128
+  String $nagios_config_file   = '/etc/nagios3/nagios.cfg',
 ) {
   require augeas
   ensure_resource('package', 'nsca-client', { ensure => present })
@@ -14,7 +15,7 @@ class nagioscfg::passive (
     command_line => "/usr/share/icinga/plugins/eventhandlers/distributed-monitoring/send_nsca_host_or_service_check_result '${nsca_server}' '/etc/send_nsca.cfg' '\$HOSTNAME\$' '\$SERVICEDESC\$' '\$SERVICESTATE\$' '\$SERVICEOUTPUT\$\\n\$LONGSERVICEOUTPUT\$|\$SERVICEPERFDATA\$'"
   } ->
   augeas { 'nagios_passive_monitor_settings':
-    incl     => "/etc/nagios3/nagios.cfg",
+    incl     => $nagios_config_file,
     lens     => 'NagiosCfg.lns',
     changes  => [
       "set enable_notifications $enable_notifications",
