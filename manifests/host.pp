7421a58629f7e27c35e8f9e14780db715ab84a5b
Micke Nordin <kano@sunet.se>
2022-03-04 11:34:18 +0100

    Make it possible to pick only on ip address
    This patch makes it possible to pick out only the first ip address
    if the host has more than one, e.g if there is both ipv4 and ipv6
    adresses for the host. It should be backwards compatible with the
    current implemantation, so that you need to explicitly set the flag
    single_ip to get this new behaviour.

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,18 +1,25 @@
 include stdlib
 include concat
+# Use a template to create a host config
+define nagioscfg::host($ensure='present', $single_ip=false) {
+  $temp_ip_list = dnsLookup($name)
+  if $single_ip {
+    $host_ip_list = [ $temp_ip_list[0] ]
+  } else {
+    $host_ip_list = $temp_ip_list
+  }
 
-define nagioscfg::host($ensure='present') {
-  $host_ip_list = dnsLookup($name)
   $host_ips = $host_ip_list ? {
     undef   => undef,
     []      => undef,
     default => join($host_ip_list, ',')
   }
+
   concat::fragment {"${nagioscfg::config}_host_${name}":
+    ensure  => $ensure,
     target  => "${nagioscfg::cfgdir}/${nagioscfg::config}_hosts.cfg",
     content => template('nagioscfg/host.erb'),
     order   => '30',
-    notify  => Service["${nagioscfg::service}"],
-    ensure  => $ensure
+    notify  => Service[$nagioscfg::service],
   }
 }
