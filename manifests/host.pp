4c6e3f8170bb3d4a58c62a16bd3d52a25f6688f0
Patrik Holmqvist <pahol@sunet.se>
2025-08-27 10:57:57 +0200

    Add option to provide a static fqdn->ip mapping.
    Some times you can not rely on DNS lookups. The example i ran in to now
    is monitoring of servers under the domain ad.vr.se that is not public
    and cant be resolved from the monitor server.

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,11 +1,21 @@
 # Use a template to create a host config
+
+# @param ip_override_map  If you for some reason can't rely on dnsLookup, you can use this parameter to provide a fqdn -> IP hash
 define nagioscfg::host(
   $single_ip                           = false,
   $action_url                          = undef,
   $sort_alphabetically                 = false,
   Optional[String] $default_host_group = undef,
   Optional[Hash] $custom_host_fields = undef,
+  Optional[Hash] $ip_override_map = undef,
 ) {
+
+  # Determine static IP list is provided, else fall back to dnsLookup
+  if $ip_override_map and $ip_override_map[$name] {
+    $unsorted_temp_ip_list = $ip_override_map[$name]
+  } else {
+    $unsorted_temp_ip_list = dnsLookup($name)
+  }
   $unsorted_temp_ip_list = dnsLookup($name)
   if $sort_alphabetically {
     $temp_ip_list = sort($unsorted_temp_ip_list)
