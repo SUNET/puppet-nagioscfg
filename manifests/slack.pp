de1b550b1f26b6aacf60cb80d88cbb51c1d5f6f3
Johannes Jeppsson <johannes@sunet.se>
2026-06-02 09:45:59 +0200

    move channel for so findable by autoloader

--- manifests/slack.pp
+++ manifests/slack.pp
@@ -1,18 +1,9 @@
 class nagioscfg::slack($domain=undef,$token=undef) {
-   file {"/usr/lib/nagios/plugins/slack_nagios.pl": 
-     ensure  => file,
-     owner   => root,
-     group   => root,
-     mode    => '0755',
-     content => template('nagioscfg/slack_nagios.erb')
-   }
-}
-
-define nagioscfg::slack::channel() {
-   nagioscfg::command {"notify-service-to-slack-$name":
-      command_line => "/usr/lib/nagios/plugins/slack_nagios.pl -field slack_channel=#$name -field HOSTALIAS='\$HOSTNAME\$' -field SERVICEDESC='\$SERVICEDESC\$' -field SERVICESTATE='\$SERVICESTATE\$' -field SERVICEOUTPUT='\$SERVICEOUTPUT\$' -field NOTIFICATIONTYPE='\$NOTIFICATIONTYPE\$'"
-   }
-   nagioscfg::command {"notify-host-to-slack-$name":
-      command_line => "/usr/lib/nagios/plugins/slack_nagios.pl -field slack_channel=#$name -field HOSTALIAS='\$HOSTNAME\$' -field HOSTSTATE='\$HOSTSTATE\$' -field HOSTOUTPUT='\$HOSTOUTPUT\$' -field NOTIFICATIONTYPE='\$NOTIFICATIONTYPE\$'"
-   }
+  file {'/usr/lib/nagios/plugins/slack_nagios.pl':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    content => template('nagioscfg/slack_nagios.erb')
+  }
 }
---
+++ manifests/slack/channel.pp
@@ -0,0 +1,8 @@
+define nagioscfg::slack::channel() {
+  nagioscfg::command {"notify-service-to-slack-${name}":
+    command_line => "/usr/lib/nagios/plugins/slack_nagios.pl -field slack_channel=#${name} -field HOSTALIAS='\$HOSTNAME\$' -field SERVICEDESC='\$SERVICEDESC\$' -field SERVICESTATE='\$SERVICESTATE\$' -field SERVICEOUTPUT='\$SERVICEOUTPUT\$' -field NOTIFICATIONTYPE='\$NOTIFICATIONTYPE\$'"
+  }
+  nagioscfg::command {"notify-host-to-slack-${name}":
+    command_line => "/usr/lib/nagios/plugins/slack_nagios.pl -field slack_channel=#${name} -field HOSTALIAS='\$HOSTNAME\$' -field HOSTSTATE='\$HOSTSTATE\$' -field HOSTOUTPUT='\$HOSTOUTPUT\$' -field NOTIFICATIONTYPE='\$NOTIFICATIONTYPE\$'"
+  }
+}
